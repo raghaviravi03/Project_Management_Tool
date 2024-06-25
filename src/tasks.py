9a00aba49551ca18606bebefc9322623cb921b17
raghaviravi03 <145817009+raghaviravi03@users.noreply.github.com>
2024-06-25 16:25:40 -0400

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -58,7 +58,7 @@ def display_task(task, email=None, company_name=None, is_admin=False, allow_stat
         st.empty()
     if email:
         st.write(task['created_at'])
-        unique_key = f"{task['_id']}-{email}-{task_index:05d}-{task['created_at'].isotime()}-{time.time()}"
+        unique_key = f"{task['_id']}-{email}-{task_index:05d}-{task['created_at'].isoformat()}-{time.time()}"
         with col9:
             view_update_btn = st.button("View/Update", key=f"view-update-{unique_key}")
         if view_update_btn:
