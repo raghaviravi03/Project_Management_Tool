3705ea4981e693f2f37841be3eca5ee7d6a025c4
raghaviravi03 <145817009+raghaviravi03@users.noreply.github.com>
2024-06-25 15:30:56 -0400

    Update tasks.py

--- src/tasks.py
+++ src/tasks.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from pymongo import DESCENDING
 import pytz
 from bson import ObjectId
+import time
 
 def truncate_text(text, max_length):
     """Truncate text and append '...' if it exceeds the max_length."""
@@ -56,7 +57,7 @@ def display_task(task, email=None, company_name=None, is_admin=False, allow_stat
     with col8:
         st.empty()
     if email:
-        unique_key = f"{task['_id']}-{email}-{task_index:05d}-{datetime.now().isoformat()}"
+        unique_key = f"{task['_id']}-{email}-{task_index:05d}-{str(time.time())}"
         with col9:
             view_update_btn = st.button("View/Update", key=f"view-update-{unique_key}")
         if view_update_btn:
