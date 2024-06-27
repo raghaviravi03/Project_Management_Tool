9e8d79ca056f41ee27379628519b1187e97186b5
raghaviravi03 <145817009+raghaviravi03@users.noreply.github.com>
2024-06-27 09:50:12 -0400

    Update admin_dashboard.py

--- src/admin_dashboard.py
+++ src/admin_dashboard.py
@@ -358,15 +358,15 @@ def display_admin_dashboard(name):
         with col1:
             lottie_json = load_lottie_file("./resources/profile.json")
             #st_lottie(
-                lottie_json,
-                speed=1,
-                reverse=False,
-                loop=True,
-                quality="high",
-                height=500,
-                width=500,
-                key="profile_animation"
-            )
+                #lottie_json,
+                #speed=1,
+                #reverse=False,
+                #loop=True,
+                #quality="high",
+                #height=500,
+                #width=500,
+                #key="profile_animation"
+            #)
 
     # elif selected_option == "Task Statistics":
     #     st.subheader("Task Statistics")
